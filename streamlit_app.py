d90574f2db06060b74dea816b77dcffb4b55c4d6
Nancy398 <nancy@moohousing.com>
2026-07-27 15:27:28 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from io import BytesIO
 # import pypandoc
 from num2words import num2words  # 用于将数字转换为英文大写
-from datetime import datetime
+from datetime import date
 import os
 from PyPDF2 import PdfReader, PdfWriter
 
